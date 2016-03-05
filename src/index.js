cf5880864c54613d8a040a9aa72f871886025654
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-03-05 08:31:27 +0000

    Adding input

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDSmIQgNwwDVMi_pHHVvUrTX6CwDD2drgA';
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 };
 
 ReactDOM.render(App(), document.querySelector('.container'));
