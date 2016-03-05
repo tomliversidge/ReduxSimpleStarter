1cebe4a414d53d928885f6925f6d8540654b409a
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-03-05 23:16:26 +0000

    Completed module 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
+import _ from 'lodash';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
 const API_KEY = 'AIzaSyDSmIQgNwwDVMi_pHHVvUrTX6CwDD2drgA';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
-};
+class App extends Component{
 
-ReactDOM.render(App(), document.querySelector('.container'));
+    constructor(props){
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('vegan');
+    }
+
+    videoSearch(term){
+        YTSearch({key:API_KEY, term:term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render(){
+        const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
