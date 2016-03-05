95754145155a7f14ed4fcee85532258fa923a2c5
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-03-05 08:59:09 +0000

    Adding listener on input change

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input/>;
-};
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = { term: '' };
+    }
+    render() {
+        return(
+            <div>
+                <input onChange={event => this.setState({ term: event.target.value})} />
+                <p>Value of the input: {this.state.term}</p>
+            </div>
+        );
+    }
+}
 
 export default SearchBar;
